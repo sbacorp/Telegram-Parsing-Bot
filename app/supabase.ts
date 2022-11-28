50177f24f8857364e57acb9793d54cde7cc9eb27
Bogdan Shankin <shankin.cpa@gmail.com>
2022-11-28 17:24:57 +0300

    123123

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,5 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 import "https://deno.land/x/dotenv/load.ts";
-import { SupabaseAdapter } from "https://deno.land/x/grammy_storage_supabase@v0.1.0/mod.ts";
 
-export const connection = createClient(Deno.env.get("SB_LINK"),Deno.env.get("SB_ANON_KEY"))
+export const connection = createClient(
+	"https://xvxxeliluchyhwncvncg.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmbHdheWRseXZvcWVhcmh2Z252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU4ODQwMjcsImV4cCI6MTk4MTQ2MDAyN30.m1Bm3JXIkzMVhFTxMlHFp59fpUMGN9Gp_tvHp43iEnA"
+);
