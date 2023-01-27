7094a129c6f5c17d69aacf86cad8dd931ddcb35d
Bogdan Shankin <shankin.dev@gmail.com>
2023-01-27 16:41:15 +0300

    456

--- app/server/db.ts
+++ app/server/db.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "npm:sequelize";
 import * as pg from 'npm:pg';
-export const sequelize = new Sequelize('parserUsers', 'bogdan', 'root', {
-	host: '77.223.106.99',
-	port: '6432',
-	dialect: 'postgres'
-  });
+export const sequelize = new Sequelize("parserUsers", "bogdan", "0lDWCy6qeZgs", {
+	host: "5.188.129.19",
+	port: "6432",
+	dialect: "postgres",
+});
