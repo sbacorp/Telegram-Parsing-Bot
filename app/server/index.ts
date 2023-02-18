528a0b339dbaf0e6d6d9d0fd8d46b514d69e19b0
sbacorp <shankin.dev@gmail.com>
2023-02-18 10:29:25 +0300

    123123

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 import { bot } from "../bot/index.ts";
 import { webhookCallback } from "https://deno.land/x/grammy@v1.12.0/mod.ts";
-import { sequelize } from "../server/db.ts";
+
 
 const handleUpdate = webhookCallback(bot, "std/http");
 serve(async (req) => {
--- app/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-import "https://deno.land/x/dotenv/load.ts";
-
-export const connection = createClient(
-	"https://jflwaydlyvoqearhvgnv.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmbHdheWRseXZvcWVhcmh2Z252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU4ODQwMjcsImV4cCI6MTk4MTQ2MDAyN30.m1Bm3JXIkzMVhFTxMlHFp59fpUMGN9Gp_tvHp43iEnA"
-);
